a7d38200bdffca2eecc3e62a2bc8633ecfc4d540
Inder <352443+InderKumarRathore@users.noreply.github.com>
2023-03-04 12:06:27 +0800

    Initial commit

---
+++ IntegrationTestsSample/ViewController.swift
@@ -0,0 +1,14 @@
+//<#Remove it#>
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
